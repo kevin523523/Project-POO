5b3d855cef9a6c063174df999f7ed85cd8e6e8b4
SaidEsquivel <104440924+SaidEsquivel@users.noreply.github.com>
2022-05-24 06:12:19 -0500

    luis

---
+++ ConexionLAN.java
@@ -0,0 +1,44 @@
+package PConexiones;
+
+
+
+/**
+ * Class Wifi
+ */
+//creado por said luis
+public class ConexionLAN {
+public static void main(String [] args){
+        Conexion p1;
+        p1 = new Conexion("luis",20);
+        
+        p1.mostrarDatos();
+    }
+    }
+  //
+  // Fields
+  //
+
+  /*
+  
+  Ver super clase Conexion 
+    
+  private String SSID;
+  private String clave;
+  private String usuario;*/
+  
+  //
+  // Constructors
+  //
+  //Herrera improvement public Wifi () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+
+
