e3eec49540bef85041351c135f664d9b79d872e0
SUAREZ205 <103235971+SUAREZ205@users.noreply.github.com>
2022-05-25 20:20:23 -0500

    Update Boton.java

--- TvDemo/src/PBotones/Boton.java
+++ TvDemo/src/PBotones/Boton.java
@@ -6,7 +6,11 @@ import PMain.Tv;
  * Class Boton
  */
 public abstract class Boton {
-
+    //@daniel suarez nava
+    public int MIN_VOLUME = 0;
+    public int MAX_VOLUME = 10;
+    //public int volume = 6;
+    public int total;
     protected Tv tv;
 
     //
