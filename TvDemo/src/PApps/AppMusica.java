1e0a1491b95fe2471197173a9d15d6de9a827cab
moonrs27 <104441686+moonrs27@users.noreply.github.com>
2022-05-26 19:09:53 -0500

    Update AppMusica.java

--- TvDemo/src/PApps/AppMusica.java
+++ TvDemo/src/PApps/AppMusica.java
@@ -1,16 +1,28 @@
 package PApps;
 
+/*
+MONTSERRAT ROJAS SANTILLAN
+paquete de la aplicacion de transmision y musica
+*/
+
 
 import PApps.App;
 
-
+/*
+MONTSERRAT ROJAS SANTILLAN
+se importa el paquete y codigo fuente de la aplicacion de transmision y musica
+*/
 
 /**
  * Class MusicaApp
  */
 public class AppMusica extends App implements Acciones{
 
-
+/*
+MONTSERRAT ROJAS SANTILLAN
+se crea la clase de app de musica con las extensiones de la aplicacion app y sus respectivas acciones
+*/
+  
   //
   // Fields
   //
@@ -37,13 +49,22 @@ public class AppMusica extends App implements Acciones{
         sot.setTitle("Musica");
        
     }
+  
+/*
+MONTSERRAT ROJAS SANTILLAN
+se implementara el metodo para abrir las interfaces de las aplicaciones de transmision y musica para asignar cambios en 
+los botones de las pestañas, que especificaciones tiene y los parametros que debera cubrir la pestaña
+*/
 
     @Override
     public void cerrar() {
         System.out.println("Cerrado de Aplicacion");
     }
 
-
+/*
+MONTSERRAT ROJAS SANTILLAN
+cierra la aplicacion con un mensaje una vez que se haya terminado la ejecucion y las acciones que se desearon hacer
+*/
   //
   // Accessor methods
   //
