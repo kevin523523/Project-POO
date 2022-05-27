71497fd5ff9a8bd57b204b6723b8357da1a92f42
moonrs27 <104441686+moonrs27@users.noreply.github.com>
2022-05-26 19:17:42 -0500

    Update AppStreaming.java

--- TvDemo/src/PApps/AppStreaming.java
+++ TvDemo/src/PApps/AppStreaming.java
@@ -1,8 +1,14 @@
 package PApps;
-
+/*
+MONTSERRAT ROJAS SANTILLAN
+paquete de la aplicacion de transmision y musica
+*/
 
 import PApps.App;
-
+/*
+MONTSERRAT ROJAS SANTILLAN
+importacion del paquete apps en el codigo App
+*/
 
 
 /**
@@ -13,7 +19,10 @@ public class AppStreaming extends App implements Acciones {
   //
   // Fields
   //
-
+/*
+MONTSERRAT ROJAS SANTILLAN
+se crea una clase para la aplicacion de la transmision con las extensiones de la aplicacion app y sus respectivas acciones
+*/
   
   //
   // Constructors
@@ -36,11 +45,20 @@ public class AppStreaming extends App implements Acciones {
         sot.setTitle("Streaming");
        
     }
+    
+/*
+MONTSERRAT ROJAS SANTILLAN
+se implementara el metodo para abrir las interfaces de las aplicaciones de transmision y musica para asignar cambios en 
+los botones de las pestañas, que especificaciones tiene y los parametros que debera cubrir la pestaña
+*/
 
     @Override
     public void cerrar() {
         System.out.println("Cerrado de Aplicacion");
-        
+/*
+MONTSERRAT ROJAS SANTILLAN
+cierra la aplicacion con un mensaje una vez que se haya terminado la ejecucion y las acciones que se desearon hacer
+*/       
         
     }
 
